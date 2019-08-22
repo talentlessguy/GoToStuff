4bd90f27b884c9d07737ed1f26cdfdfea6cf2518
talentlessguy <pilll.PL22@gmail.com>
2019-08-22 09:33:16 +0300

    map, foldr, foldl

--- racket/lambdas.rkt
+++ racket/lambdas.rkt
@@ -6,16 +6,21 @@
     (cons (f (first l)) (myMap (rest l) f))
 ))
 
-(define (myFoldl l f acc)
+(define (myFoldl f acc l)
   (if (null? l)
     acc
-    (myFoldl f (
-      (f acc (first l))
-      (rest l)
-    ))
+    (foldl f (f acc (first l))
+      (rest l))
+))
+
+(define (myFoldr f acc l)
+  (if (null? l)
+    acc
+    (f (myFoldr f acc (rest l))
+      (first l))
 ))
 
 (myMap '(1 2 3) (lambda (x) (+ x 1)))
-(myFoldl '(1 2 3 4) 
-  (lambda (x y) (+ x y )) 1
-)
+
+(foldl + 0 '(1 2 3))
+(foldr + 0 '(1 2 3))
